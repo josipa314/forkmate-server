fedf4033d0fc26d0ef69a0aac04a9a8a8254dee0
Josipa314 <101945658+josipa314@users.noreply.github.com>
2022-05-24 16:17:45 +0200

    small change

--- app.js
+++ app.js
@@ -21,7 +21,6 @@ app.use("/api", require("./routes/index.routes"));
 app.use("/api", require("./routes/meal.routes"));
 app.use("/api", require("./routes/company.routes")); 
 
-
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require("./error-handling")(app);
 
